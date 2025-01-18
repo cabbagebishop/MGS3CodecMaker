9a48445a9b4d386f3048b08b6c152077dcbb1c46
Alek Bouillon <cabbagefiles2016@gmail.com>
2025-01-17 21:02:55 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-This is a placeholder
+metal-gear-codec
+================
+
+Click Change Image to insert a custom image.
+
+Enter your custom text in the text area.
+
+Click Download PNG to save the image as a PNG file.
+
+**Demo:** https://cabbagebishop.github.io/MGS3CodecMaker/
---
+++ mgs-player.js
@@ -0,0 +1,62 @@
+/*!
+* Metal Gear Solid Codec.
+* (c) 2013 Chris Tabor <dxdstudio@gmail.com>
+* See license for more information
+* <3
+* https://github.com/christabor/metal-gear-codec/
+*/
+;(function($){
+	$.fn.mgsCodec = function(options) {
+		var defaults = {
+			interval_speed: 300,
+			animation_timeout: 1500,
+			transcription: '.transcription p'
+		},
+		opts = $.extend(defaults, options),
+		self = this,
+		notes = this.find(opts.transcription),
+		current_note = 0,
+		volume_indicator = this.find('#svg-volume-indicator-total'),
+		max_volume = volume_indicator.height();
+		notes.hide();
+
+		this.find('img').hide();
+
+		function triggerClick() {
+			// advance dialogue to next note
+			notes.eq(current_note).fadeIn(200);
+
+			// hide previous
+			notes.eq(current_note).prevAll().hide(100);
+
+			// increment forward
+			current_note += 1;
+			return;
+		}
+
+		function animateCodecBar() {
+			// randomize the height of the bar to simulate volume
+			volume_indicator.height(Math.random()*max_volume);
+			return;
+		}
+
+		function init() {
+			self.find('img').each(function(k, elem){
+				var _elem = $(elem);
+				_elem.fadeIn(400);
+			});
+
+			// show first note
+			notes.eq(0).show();
+
+			self.on('click', triggerClick);
+
+			setTimeout(function(){
+				setInterval(animateCodecBar, opts.interval_speed);
+			}, opts.animation_timeout);
+			return;
+		}
+
+		this.hide().slideToggle(200, init);
+	};
+})(jQuery);
